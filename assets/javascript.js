1b0488e9738db87be2cbeb3f9767f2e5d0e4d7d0
Arjun Singh <arjunkhakh@gmail.com>
2022-01-22 18:20:31 +0000

    Working on the local storage for JS

--- assets/javascript.js
+++ assets/javascript.js
@@ -29,23 +29,28 @@ $(hour).each(function(i)
  $(document).ready(function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+    // $(".saveBtn").on("click", function(){
+    // var plannerText = $(this).siblings(".description").val();
+    // var time = $(this).siblings().attr("data-id");
+    // localStorage.setItem(time, plannerText);
+
     $(button).on("click", function(){
-    var plannerText = $(button).siblings(".description").val();
-    var time = $(button).siblings(".description").attr("id");
-    localStorage.setItem(time, plannerText);
+        var plannerText = $(button).siblings(".description").val();
+        var time = $(button).parent().attr("id");
+        localStorage.setItem(time, plannerText);
 
 })
 
- $("#9am.description").val(localStorage.getItem("9am"));
- $("#10am.description").val(localStorage.getItem("10am"));
- $("#11am.description").val(localStorage.getItem("11am"));
- $("#12pm.description").val(localStorage.getItem("12pm"));
- $("#1pm.description").val(localStorage.getItem("1pm"));
- $("#2pm.description").val(localStorage.getItem("2pm"));
- $("#3pm.description").val(localStorage.getItem("3pm"));
- $("#4pm.description").val(localStorage.getItem("4pm"));
- $("#5pm.description").val(localStorage.getItem("5pm"));
- $("#6pm.description").val(localStorage.getItem("6pm"));
+ $("#9am. description").val(localStorage.getItem("9am"));
+ $("#10am. description").val(localStorage.getItem("10am"));
+ $("#11am. description").val(localStorage.getItem("11am"));
+ $("#12pm. description").val(localStorage.getItem("12pm"));
+ $("#1pm. description").val(localStorage.getItem("1pm"));
+ $("#2pm. description").val(localStorage.getItem("2pm"));
+ $("#3pm. description").val(localStorage.getItem("3pm"));
+ $("#4pm. description").val(localStorage.getItem("4pm"));
+ $("#5pm. description").val(localStorage.getItem("5pm"));
+ $("#6pm. description").val(localStorage.getItem("6pm"));
  
 });
 
