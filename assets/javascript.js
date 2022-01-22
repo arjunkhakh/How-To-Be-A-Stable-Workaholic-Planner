98c17d6935ff99a0d502fb1b90930b455eaafb5f
Arjun Singh <arjunkhakh@gmail.com>
2022-01-22 20:38:17 +0000

    Added Comments to the JS Page

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,30 +1,29 @@
+// Variables Defined Here including an array being defined as a variable
 var date = document.querySelector("#currentDay");
-
 var textArea = document.querySelectorAll(".textarea");
-
 var container = document.querySelector(".container")
-
 var hour = document.querySelectorAll(".hour") 
-
 var button = document.querySelectorAll(".saveBtn")
-
-date.textContent = moment().format("dddd DDDo MMMM YYYY")
-
 var time = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
 
+// The Exact date at the moment
+date.textContent = moment().format("dddd DDDo MMMM YYYY")
+
+// A Function for the array which puts in the times from the array and place them in each time block
 $(hour).each(function(i)
 {
      $(this).attr("data-id",time[i]);
       $(this).html(time[i]);
  });
 
+ // A Function to clear the contents on the page and on the local storage
  function clearStorage(event) {
      localStorage.clear()
      document.location.reload();
  }
 
+ // When the document is ready, it will make the save buttons put the input into local storage with the time and information inside it.
  $(document).ready(function() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
     $(".saveBtn").on("click", function(){
         var plannerText = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -32,9 +31,11 @@ $(hour).each(function(i)
 
     })
  
+    // In this for loop, the first part is making sure the information stays on the page and in local storage.
 for (i = 9; i < 19; i++) {
     $("#" + i + " .description").val(localStorage.getItem(i));
 
+    // for this if statement, it gets a defined variable of the exact time and changes the background colour of the block to according what time it is.
     var t = new Date().getHours();
 
     if (t = i) {
